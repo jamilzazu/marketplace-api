7312695cef8fada623300751338611f4a1b80776
Maicon Silva <maiconrs95@gmail.com>
2018-12-06 00:02:26 -0200

    Adicionando método estático na model User para gerar o token de autenticação

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../../config/auth')
 
 const UserSchema = new mongoose.Schema(
   {
@@ -38,5 +40,25 @@ UserSchema.pre('save', async function (next) {
 
   this.password = await bcrypt.hash(this.password, 8)
 })
+
+/**
+ * @description: Método chamado no momento do login para verificar se a senha informada no body(hash) é
+ * igual a senha do usuário cadastrada no banco de dados
+ */
+UserSchema.methods = {
+  compareHash (password) {
+    return bcrypt.compare(password, this.password)
+  }
+}
+
+// Métodos estáticos podem ser chamados sem a instância da classe
+UserSchema.statics = {
+  // Gera o token de autenticação do usuário
+  generateToken ({ id }) {
+    return jwt.sign({ id }, authConfig.secret, {
+      expiresIn: authConfig.ttl
+    })
+  }
+}
 
 module.exports = mongoose.model('User', UserSchema)
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ const express = require('express')
 const routes = express.Router()
 
 // Controllers
-const UserController = require('./app/controllers/User')
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
 // User routes
 routes.post('/users', UserController.store)
+
+// Auth
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes
